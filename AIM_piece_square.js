1f2ed331e7b79a6dadc65df81bae9de027d36298
Ishhana <ishhanajegan@gmail.com>
2021-11-05 14:04:01 +0800

    Create AIM_piece_square.js
    halo

---
+++ AIM_piece_square.js
@@ -0,0 +1,141 @@
+//Evaluate board using piece-square tables in a 8x8 matrix
+var evaluate_board = function(board)
+{
+    var total_evaluation = 0;
+    for (var i = 0; i < 8; i++)
+    {
+        for (var j = 0; j < 8; j++)
+        {
+            total_evaluation = total_evaluation + getPieceVal
+            (board[i][j], i, j); 
+        }
+    }
+    return total_evaluation;
+};
+
+var reverse_array = function (array)
+{
+    return array.slice().reverse();
+};
+
+var pawn_white_eval = 
+[
+    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+    [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+    [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+    [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+    [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+    [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+];
+
+var pawn_black_eval = reverse_array(pawn_white_eval);
+
+var knight_eval = 
+[
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+    [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+    [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+    [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+    [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+    [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+    [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+];
+
+var bishop_white_eval = 
+[
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+];
+
+var bishop_black_eval = reverse_array (bishop_white_eval);
+
+var rook_white_eval = 
+[
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+];
+
+var rook_black_eval = reverse_array(rook_white_eval);
+
+var queen_eval = 
+[
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+];
+
+var king_white_eval = 
+[
+
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0]
+];
+
+var king_black_eval = reverse_array(king_white_eval);
+
+var getPieceVal = function (piece , x, y) {
+{
+    if (piece === null)
+    {
+        return 0;
+    }
+}
+
+    var getAbsoluteVal = function (piece , is_white, x, y)
+    {
+        if (piece.type === 'p') 
+        {
+            return 10 + (is_white ? pawn_white_eval[y][x] : pawn_black_eval[y][x]);
+        }
+        else if (piece.type === 'r')
+        {
+            return 50 + (is_white ? rook_white_eval[y][x] : rook_black_eval[y][x]);
+        }
+        else if (piece.type === 'n')
+        {
+            return 30 + knight_eval[y][x];
+        }
+        else if (piece.type === 'b')
+        {
+            return 30 + (is_white ? bishop_white_eval[y][x] : bishop_black_eval[y][x]);
+        }
+        else if (piece.type === 'q')
+        {
+            return 90 + queen_eval[y][x];
+        }
+        else if (piece.type === 'k')
+        {
+            return 900 + (is_white ? king_white_eval[y][x] : king_black_eval[y][x]);
+        }
+        throw "Piece Type Unknown: " + piece.type;    
+    };
+
+    var absolute_value = getAbsoluteVal(piece, piece.color === 'z', x, y);
+    return piece.color === 'z' ? absolute_value : -absolute_value;
+};
